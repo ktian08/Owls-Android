35889d254f1313214c05ea55d71a61d7e11a1635
Kane Tian <kanetian7@gmail.com>
2017-05-11 21:32:02 -0700

    Created the ground platform, about to change it to sprite

--- core/src/com/mygdx/game/OwlsGame.java
+++ core/src/com/mygdx/game/OwlsGame.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class OwlsGame extends ApplicationAdapter {
@@ -21,14 +21,13 @@ public class OwlsGame extends ApplicationAdapter {
 	private World world;
 	private Body body;
 	private OrthographicCamera camera;
-	private BitmapFont font;
 	private Box2DDebugRenderer debugRenderer;
 	private Matrix4 debugMatrix;
+	private ShapeRenderer shapeRenderer;
 
-	private static final float WORLD_WIDTH = 80;
-	private static final float WORLD_HEIGHT = 45;
-
-	private static final float PIXELS_TO_METERS = 50f;
+	private float width;
+	private float height;
+	private float heightGround;
 	
 	@Override
 	public void create () {
@@ -36,26 +35,28 @@ public class OwlsGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		//create world with -10 gravity in vert direction
 		world = new World(new Vector2(0.0f, -10.0f), true);
-		//create ground platform
+		//initialize width and height (of screen)
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		//create ground platform physics box
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyDef.BodyType.StaticBody;
-		bodyDef.position.set(0,0);
+		heightGround = height/10;
+		bodyDef.position.set(0, -height/2+heightGround/2);
 
 		FixtureDef fixtureDef = new FixtureDef();
-		EdgeShape edge = new EdgeShape();
-		float w = Gdx.graphics.getWidth()/PIXELS_TO_METERS;
-		float h = Gdx.graphics.getHeight()/PIXELS_TO_METERS-50/PIXELS_TO_METERS;
-		edge.set(-w/2, -h/2, w/2, -h/2);
-		fixtureDef.shape = edge;
+		PolygonShape ground = new PolygonShape();
+		ground.setAsBox(width, heightGround/2);
+		fixtureDef.shape = ground;
 
 		body = world.createBody(bodyDef);
 		body.createFixture(fixtureDef);
-		edge.dispose();
+
+		ground.dispose(); //dispose of box shape, body already created
+		//create shape renderer
+		shapeRenderer = new ShapeRenderer();
 		//set debug renderer
 		debugRenderer = new Box2DDebugRenderer();
-		//pick font
-		font = new BitmapFont();
-		font.setColor(Color.BLACK);
 		//set camera
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
@@ -70,19 +71,28 @@ public class OwlsGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//change batch to perspective of camera
 		batch.setProjectionMatrix(camera.combined);
-		debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS,
-				PIXELS_TO_METERS, 0);
+		debugMatrix = batch.getProjectionMatrix().cpy();
 		//execute
 		batch.begin();
 		batch.end();
-
+		//debug physics boxes/bodies
 		debugRenderer.render(world, debugMatrix);
+		//draw ground platform rectangle
+		shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.rect(-width/2, -height/2, width, heightGround);
+		shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
+		//prevent memory leaks!!!
 		batch.dispose();
 		world.dispose();
+		shapeRenderer.dispose();
+		debugRenderer.dispose();
 	}
 
 	@Override
